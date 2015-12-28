b1b92b6aa369b51919e34c03afed12454c78aabf
Ryze <Ryze@xigualicai.com>
2015-12-28 20:30:50 +0800

    update

---
+++ src/main/java/com/xyy/boot/aop/ServiceMonitor.java
@@ -0,0 +1,15 @@
+package com.xyy.boot.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class ServiceMonitor {
+	@AfterReturning("execution(* com.xyy.boot.service.impl.*Impl.*(..))")
+	public void logServiceAccess(JoinPoint joinPoint) {
+		System.out.println("Completed: " + joinPoint);
+	}
+}
